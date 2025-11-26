61e0cbe8fdcb072cd5f0daff4bcbc3cf11b0d764
vincentgraillat <vincent@nelson-mobility.com>
2025-11-26 21:49:16 +0100

    Create user model

---
+++ backend/models/user.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass, field
+from uuid import uuid4
+
+
+@dataclass
+class User:
+    id: str = field(default_factory=lambda: str(uuid4()))
+    name: str = ""
+    email: str | None = None
+
+    def __str__(self) -> str:
+        return f"{self.name} ({self.id})"
